3dbffc1b7b00c05b4edd716417f738b97290905e
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-26 19:42:09 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -21,7 +21,7 @@ class EmailParser
   def parse
     emails_seperated = self.emails.split(/\W\s|\s/)
     emails_seperated.each do |email_unsaved|
-      if self.all.detect {|email_saved| email_saved == email_unsaved} == nil
+      if self.classall.detect {|email_saved| email_saved == email_unsaved} == nil
         self.class.all << email_unsaved
       end
     end
