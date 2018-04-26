0b639c869917359771cd1eb7858d3af817918065
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-26 19:33:04 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,14 +7,21 @@ require 'pry'
 class EmailParser
 
   attr_accessor :emails
+  @all = []
 
   def initialize(csv)
     @emails = csv
+    @all << @emails
     @emails
+  end
+
+  def self.all
+    @@all
   end
 
   def parse
     self.emails.split(/\W\s|\s/)
+    
   end
 
 end
